85bc1e111ef4d5489d4340e59efdc45d455d50fb
harshilJs <harshilame@gmail.com>
2021-04-06 14:23:42 +0530

    init

---
+++ App.web.js
@@ -0,0 +1,44 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('./logo.png')}
+        style={{height: 100, width: 100, resizeMode: 'contain'}}
+      />
+      <Text style={styles.title}>Hello from {'\n'}React Native Web!</Text>
+      <TouchableOpacity
+        onPress={() => setCount(count + 1)}
+        style={styles.button}>
+        <Text>Click me!</Text>
+      </TouchableOpacity>
+
+      <Text>You clicked {count} times!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#C3E8BD',
+    paddingTop: 40,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: '#ADBDFF',
+    padding: 5,
+    marginVertical: 20,
+    alignSelf: 'flex-start',
+  },
+  title: {
+    fontSize: 40,
+    paddingTop: 40,
+  },
+});
+
+export default App;
